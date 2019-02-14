382bdd2409e3810cc5d7b8bca2dfe6706bc70dd8
DK\dmsa <dmsa@sitecore.net>
2019-02-14 09:08:46 +0200

    Fix the issue #310438.

---
+++ src/Sitecore.Support.310438/ExperienceForms/Mvc/Filters/ValidateFormRequestAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Diagnostics;
+using System;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.ExperienceForms.Mvc.Filters
+{
+  [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+  internal class ValidateFormRequestAttribute : FilterAttribute, IAuthorizationFilter
+  {
+    private readonly ValidateFormRequestContext _validationContext;
+
+    public ValidateFormRequestAttribute() : this(new ValidateFormRequestContext())
+    {
+    }
+
+    internal ValidateFormRequestAttribute(ValidateFormRequestContext validateFormAntiForgeryContext)
+    {
+      Assert.ArgumentNotNull(validateFormAntiForgeryContext, "validateFormAntiForgeryContext");
+      this._validationContext = validateFormAntiForgeryContext;
+    }
+
+    public void OnAuthorization(AuthorizationContext filterContext)
+    {
+      Assert.ArgumentNotNull(filterContext, "filterContext");
+      string str = this._validationContext.FormRenderingContext.CreatePropertyName("FormItemId");
+      if (!string.IsNullOrEmpty(filterContext.RequestContext.HttpContext.Request.Form[str]) || !this._validationContext.IsExperienceEditor)
+      {
+        this._validationContext.Validate();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.310438/ExperienceForms/Mvc/Filters/ValidateFormRequestContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.ExperienceForms.Mvc;
+using System;
+using System.Web.Helpers;
+
+namespace Sitecore.Support.ExperienceForms.Mvc.Filters
+{
+  internal class ValidateFormRequestContext
+  {
+    internal virtual bool IsExperienceEditor
+    {
+      get
+      {
+        return Context.PageMode.IsExperienceEditor;
+      }
+    }
+
+    internal virtual IFormRenderingContext FormRenderingContext
+    {
+      get
+      {
+        return ServiceProviderServiceExtensions.GetService<IFormRenderingContext>(ServiceLocator.ServiceProvider);
+      }
+    }
+
+    internal virtual Action Validate
+    {
+      get
+      {
+        return new Action(AntiForgery.Validate);
+      }
+    }
+  }
+}
