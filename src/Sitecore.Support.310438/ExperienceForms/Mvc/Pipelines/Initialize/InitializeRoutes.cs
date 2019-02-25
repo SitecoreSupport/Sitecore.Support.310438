be27a0675739025aba2c503a5f62ae6f05dbf21e
DK\dmsa <dmsa@sitecore.net>
2019-02-25 13:51:50 +0200

    Update the patch In order to avoid an error which is related to the fact that multiply controllers were fount.

---
+++ src/Sitecore.Support.310438/ExperienceForms/Mvc/Pipelines/Initialize/InitializeRoutes.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using InitializeRoutesBase = Sitecore.Mvc.Pipelines.Loader.InitializeRoutes;
+
+namespace Sitecore.Support.ExperienceForms.Mvc.Pipelines.Initialize
+{
+  public class InitializeRoutes : InitializeRoutesBase
+  {
+    protected override void RegisterRoutes(RouteCollection routes, PipelineArgs args)
+    {
+      routes.MapRoute("FormBuilder", "FormBuilder/{action}/{id}", new
+      {
+        controller = "FormBuilder",
+        action = "Index",
+        id = UrlParameter.Optional
+      },
+      new string[] { "Sitecore.Support.ExperienceForms.Mvc.Controllers" });
+
+      routes.MapRoute("FieldTracking", "fieldtracking/{action}", new
+      {
+        controller = "FieldTracking",
+        action = "Register"
+      });
+    }
+  }
+}
